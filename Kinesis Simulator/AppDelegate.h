e776b605a6f15cb35ac1a72cccf895a251ab3a37
Achal Aggarwal <aggarwal.achal@gmail.com>
2012-05-28 23:04:34 +0530

    Added basic button events

--- Kinesis Simulator/AppDelegate.h
+++ Kinesis Simulator/AppDelegate.h
@@ -9,14 +9,17 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSView *handTrackingView;
+
+@property (weak) IBOutlet NSTabView *tabView;
+
 @property (weak) IBOutlet NSButton *startServerButton;
 @property (weak) IBOutlet NSButton *stopServerButton;
 @property (weak) IBOutlet NSButton *connectKinectButton;
 @property (weak) IBOutlet NSButton *disconnectKinectButton;
 
-@property (assign) IBOutlet NSWindow *window;
-
-@property (weak) IBOutlet NSView *handTrackingView;
 - (IBAction)startServer:(id)sender;
 - (IBAction)stopServer:(id)sender;
 - (IBAction)connectKinect:(id)sender;
--- Kinesis Simulator/AppDelegate.m
+++ Kinesis Simulator/AppDelegate.m
@@ -16,6 +16,7 @@
 @synthesize disconnectKinectButton = _disconnectKinectButton;
 @synthesize window = _window;
 @synthesize handTrackingView = _handTrackingView;
+@synthesize tabView = _tabView;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -24,14 +25,45 @@
 }
 
 - (IBAction)startServer:(id)sender {
+    //start server
+    
+    [[self startServerButton] setEnabled:NO];
+    [[self stopServerButton] setEnabled:YES];
+    [[self connectKinectButton] setEnabled:YES];
+    
+    
 }
 
 - (IBAction)stopServer:(id)sender {
+    [[self startServerButton] setEnabled:YES];
+    [[self stopServerButton] setEnabled:NO];
+    
+    [self disconnectKinect:nil];
+    
+    [[self connectKinectButton] setEnabled:NO];
+
+    //disconnect kinect
+    //stop server
 }
 
 - (IBAction)connectKinect:(id)sender {
+    [[self connectKinectButton] setEnabled:NO];
+    [[self disconnectKinectButton] setEnabled:YES];
+    
+    if ([[self tabView] isHidden]) {
+        [[self tabView] setHidden:NO];
+    }
+    //connect Kinect
 }
 
 - (IBAction)disconnectKinect:(id)sender {
+    [[self disconnectKinectButton] setEnabled:NO];
+    [[self connectKinectButton] setEnabled:YES];
+    
+    if (![[self tabView] isHidden]) {
+        [[self tabView] setHidden:YES];
+    }
+    
+    //disconnect Kinect
 }
 @end
